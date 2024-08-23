28a24cb59fe97bfef379e5655a6a50ea254f001a
alishazin <78612608+alishazin@users.noreply.github.com>
2024-08-23 18:48:46 +0530

    n queen is done

--- 7/main.py
+++ 7/main.py
@@ -6,40 +6,44 @@ class NQueenProblem:
         self.N = n
         self.board = [[0 for j in range(self.N)] for i in range(self.N)]
 
+
     def printBoard(self):
         
         for i in range(self.N):
             for j in range(self.N):
-                print(". " if self.board[i][j] == 0 else "Q ", end="")
-            print()
+                print(".  " if self.board[i][j] == 0 else "Q  ", end="")
+            print("\n")
+
 
     def isSafe(self, row, col):
 
         # Check Same Column and Row
-        # for i in range(self.N):
-        #     if self.board[i][col] == 1:
-        #         return False
-        #     if self.board[row][i] == 1:
-        #         return False
+        for i in range(self.N):
+            if self.board[i][col] == 1:
+                return False
+            if self.board[row][i] == 1:
+                return False
 
         # Check Diagonal (Top Left to Bottom Right)
         for i, j in zip(
                 range(row - min(row, col), row + (self.N - max(row, col))), 
                 range(col - min(row, col), col + (self.N - max(row, col)))
             ):
-            pass
-            # print(i, j)
+            if self.board[i][j] == 1: return False
 
         # Check Diagonal (Bottom Left to Top Right)
         for i, j in zip(
-                range(), 
-                range()
+                range(row + min(self.N - 1 - row, col), row - min(self.N - 1 - col, row) - 1, -1),
+                range(col - min(self.N - 1 - row, col), col + min(self.N - 1 - col, row) + 1)
             ):
-            print(i, j)
+            if self.board[i][j] == 1: return False
+
+        return True
+
 
     def solve(self, row=0):
 
-        if row == self.N:
+        if row >= self.N:
             return True
         
         for i in range(self.N):
@@ -48,7 +52,17 @@ class NQueenProblem:
 
                 self.board[row][i] = 1
 
+                if self.solve(row+1) == True:
+                    return True
+
+                self.board[row][i] = 0
+        
+        return False
 
 
 obj = NQueenProblem(4)
-obj.isSafe(0,1)
+
+if obj.solve():
+    obj.printBoard()
+else:
+    print("Solution does not exist")
