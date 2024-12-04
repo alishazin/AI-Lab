9316ee7451e2e4df65aeedec58ac15109bda6842
alishazin <78612608+alishazin@users.noreply.github.com>
2024-12-04 13:19:01 +0530

    done

--- naive/main.py
+++ naive/main.py
@@ -1,78 +1,10 @@
 
 import pandas as pd
-from naive_bayes import MultinomialNaiveBayesClassifier, GaussianNaiveBayesClassifier
-from testing import k_fold_cross_validation
+from naive_bayes import GaussianNaiveBayesClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-
-""" Salary Data """
-
-# df = pd.read_csv('datasets/salary.csv')
-
-# mnb = MultinomialNaiveBayesClassifier()
-
-# X = df.iloc[:, 0:-1]
-# Y = df.iloc[:, -1]
-
-# mnb.fit(X, Y)
-
-# y_test = pd.DataFrame(
-#     [
-#         ["facebook","computer programmer","bachelors"],
-#         ["abc pharma","business manager","bachelors"],
-#     ], 
-#     columns=["company","job","degree"]
-# )
-
-
-# y_pred =  mnb.predict(y_test)
-# print(y_pred)
-
-""" PlayTennis Data """
-
-# df = pd.read_csv('datasets/data.csv')
-
-# mnb = MultinomialNaiveBayesClassifier(alpha=1)
-
-# X = df.iloc[:, 1:-1]
-# Y = df.iloc[:, -1]
-
-# mnb.fit(X, Y)
-
-# y_test = pd.DataFrame(
-#     [
-#         ["Sunny", "Cool", "High", "Strong"],
-#         ["Overcast", "Hot", "High", "Strong"],
-#     ], 
-#     columns=["Outlook", "Temperature", "Humidity", "Wind"]
-# )
-
-# y_pred =  mnb.predict(y_test)
-# print(y_pred)
-
-""" Gender  Data """
-
-# df = pd.read_csv('datasets/gender.csv')
-
-# gnb = GaussianNaiveBayesClassifier()
-
-# X = df.iloc[:, 0:-1]
-# Y = df.iloc[:, -1]
-
-# gnb.fit(X, Y)
-
-# y_test = pd.DataFrame(
-#     [
-#         [6, 130, 8],
-#     ], 
-#     columns=["Height", "Weight", "Foot Size"]
-# )
-
-# y_pred =  gnb.predict(y_test)
-# print(y_pred)
-
-""" Iris Dataset """
 from sklearn.datasets import load_iris 
+
 df = load_iris()
 
 X = pd.DataFrame(df.data)
@@ -96,6 +28,3 @@ plt.show()
 from sklearn.metrics import accuracy_score 
 ac = accuracy_score(y_test, y_pred)
 print(ac)
-
-# score = k_fold_cross_validation(gnb.fit, gnb.predict, gnb.reset, X, y, 10)
-# print(score)
